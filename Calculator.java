13b53b4151fba97fc4824ff12b60faf531d743da
Vrushab Shreenidhi <65905866+VrushabS@users.noreply.github.com>
2025-11-10 20:45:00 -0500

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,19 @@
+public class Calculator {
+    public Calculator(){
+
+    }
+    // add method
+    public int add(int a, int b) {return a + b;}
+
+    // subtract method
+    public int subtract(int a, int b) {return a - b;}
+
+    // multiply method
+    public int multiply(int a, int b) {return a * b;}
+
+    // divide method
+    public int divide(int a, int b) {return a / b;}
+
+    // modulo method
+    public int modulo(int a, int b) {return a % b;}
+}
---
+++ Main.java
@@ -0,0 +1,4 @@
+public static void main(String[] args) {
+    Calculator myCalculator = new Calculator();
+    System.out.println(myCalculator.add(5, 7));
+}
